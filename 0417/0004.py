911f6013d667e6ef80ccbebabfbaf7e9dce4167b
Ryujongheon <lptprjh@bufs.ac.kr>
2019-04-28 08:26:57 +0900

    0417 풀이 개선 빈 줄에도 대응

--- 0417/0004.py
+++ 0417/0004.py
@@ -1,6 +1,10 @@
 total = 0
 i = -1
 while i != 0:
-    i = int(input("숫자를 입력하세요: "))
+    try: i = int(input("숫자를 입력하세요: "))
+    # 빈 줄/문자열 등 정수로 변환할 수 없는 값을 입력할 경우에도 0을 입력한 것과 같은 효과를 냄
+    except ValueError: break
+    
     if i>0: total += i
+
 print(total)
