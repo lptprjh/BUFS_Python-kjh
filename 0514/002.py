db1f2a6133a72f3bcc6143b48af15ee39ecf53ce
Ryu jongheon <lptprjh@gmail.com>
2019-05-14 14:49:48 +0900

    /0514/002 답안 수정

--- 0514/002.py
+++ 0514/002.py
@@ -1,32 +1,25 @@
 import turtle
 import random
 
-def initTurtle(shape,x,y):
+def initTurtle(shape,color,x,y):
     t = turtle.Turtle()
+    t.color(color)
     t.shape(shape)
     t.pensize(5)
+    t.shapesize(5)
     t.penup()
     t.goto(x,y)
     return t
 
 screen = turtle.Screen()
-# image1 = ""
-# image2 = ""
-# image3 = ""
-# screen.addshape(image1)
-# screen.addshape(image2)
-# screen.addshape(image3)
 
-t1 = initTurtle("turtle",-300,+100)
-t2 = initTurtle("turtle",-300,0)
-t3 = initTurtle("turtle",-300,-100)
+t1 = initTurtle("turtle","pink",-300,0)
+t2 = initTurtle("turtle","blue",-300,-100)
 
 t1.pendown()
 t2.pendown()
-t3.pendown()
 t1.speed(1)
 t2.speed(1)
-t3.speed(1)
 
 for i in range(100):
     d1 = random.randint(1,6)
@@ -34,6 +27,3 @@ for i in range(100):
 
     d2 = random.randint(1,6)
     t2.forward(d2)
-    
-    d3 = random.randint(1,6)
-    t3.forward(d3)
